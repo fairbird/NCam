fairbird/NCam

commit 18264a42f3a55d7ad33f9710a4538ef600181e8d

cacheex: Cleanup ifdefs in oscam.c
No binary changes.

git-svn-id: http://www.streamboard.tv/svn/oscam/trunk@7674 4b0bc96b-bc66-0410-9d44-ebda105a78c1

oscam.c
@@ -320,9 +320,7 @@ static void remove_ecm_from_reader(ECM_REQUEST *ecm) {
 	            	if (er->parent == ecm) {
 	            		er->parent = NULL;
 	            		er->client = NULL;
-#ifdef CS_CACHEEX
-	            		er->csp_lastnodes = NULL;
-#endif
+	            		cacheex_set_csp_lastnode(NULL);
 	            	}
 	            }
             }
@@ -335,11 +333,8 @@ static void remove_ecm_from_reader(ECM_REQUEST *ecm) {
 void free_ecm(ECM_REQUEST *ecm) {
 	struct s_ecm_answer *ea, *nxt;
 
-#ifdef CS_CACHEEX
-        LLIST *l = ecm->csp_lastnodes;
-        ecm->csp_lastnodes = NULL;
-	ll_destroy_data(l);
-#endif
+	cacheex_free_csp_lastnodes(ecm);
+
 	//remove this ecm from reader queue to avoid segfault on very late answers (when ecm is already disposed)
 	//first check for outstanding answers:
 	remove_ecm_from_reader(ecm);
@@ -395,10 +390,7 @@ static void cleanup_ecmtasks(struct s_client *cl)
 	for (ecm = ecmcwcache; ecm; ecm = ecm->next) {
 		if (ecm->client == cl)
 			ecm->client = NULL;
-#ifdef CS_CACHEEX
-		if (ecm->cacheex_src == cl)
-			ecm->cacheex_src = NULL;
-#endif
+		cacheex_set_cacheex_src(ecm, cl);
 		//if cl is a reader, remove from matching_rdr:
 		for(ea_list = ecm->matching_rdr, ea_prev=NULL; ea_list; ea_prev = ea_list, ea_list = ea_list->next) {
 			if (ea_list->reader->client == cl) {
@@ -1259,10 +1251,7 @@ static void distribute_ecm(ECM_REQUEST *er, int32_t rc)
 	cs_readlock(&ecmcache_lock);
 	for (ecm = ecmcwcache; ecm; ecm = ecm->next) {
 		if (ecm != er && ecm->rc >= E_99 && ecm->ecmcacheptr == er) {
-#ifdef CS_CACHEEX
-			if (!ecm->cacheex_src)
-				ecm->cacheex_src = er->cacheex_src;
-#endif
+			cacheex_init_cacheex_src(ecm, er);
 			write_ecm_answer(er->selected_reader, ecm, rc, 0, er->cw, NULL);
 		}
 	}
@@ -2305,11 +2294,7 @@ void get_cw(struct s_client * client, ECM_REQUEST *er)
 	}
 
 	struct s_ecm_answer *ea, *prv = NULL;
-#ifdef CS_CACHEEX
 	if(er->rc >= E_99 && !cacheex_is_match_alias(client, er)) {
-#else
-	if(er->rc >= E_99) {
-#endif
 		er->reader_avail=0;
 		struct s_reader *rdr;
 

module-cacheex.c
@@ -223,7 +223,7 @@ static struct s_cacheex_matcher *is_cacheex_matcher_matching(ECM_REQUEST *from_e
 	return NULL;
 }
 
-int8_t cacheex_is_match_alias(struct s_client *cl, ECM_REQUEST *er) {
+bool cacheex_is_match_alias(struct s_client *cl, ECM_REQUEST *er) {
 	return cl && cl->account && cl->account->cacheex == 1 && is_cacheex_matcher_matching(NULL, er);
 }
 

module-cacheex.h
@@ -8,7 +8,6 @@ extern uint8_t cacheex_peer_id[8];
 extern int32_t cacheex_add_stats(struct s_client *cl, uint16_t caid, uint16_t srvid, uint32_t prid, uint8_t direction);
 extern int8_t cacheex_maxhop(struct s_client *cl);
 void cacheex_cache_push(ECM_REQUEST *er);
-extern int8_t cacheex_is_match_alias(struct s_client *cl, ECM_REQUEST *er);
 extern int8_t cacheex_match_alias(struct s_client *cl, ECM_REQUEST *er, ECM_REQUEST *ecm);
 extern void cacheex_add_to_cache(struct s_client *cl, ECM_REQUEST *er);
 extern void cacheex_add_to_cache_from_csp(struct s_client *cl, ECM_REQUEST *er);
@@ -19,6 +18,27 @@ extern void cacheex_clear_client_stats(struct s_client *client);
 extern void cacheex_load_config_file(void);
 extern void cacheex_update_hash(ECM_REQUEST *er);
 static inline bool cacheex_reader(struct s_reader *rdr) { return rdr->cacheex == 1; };
+extern bool cacheex_is_match_alias(struct s_client *cl, ECM_REQUEST *er);
+static inline void cacheex_set_csp_lastnode(ECM_REQUEST *er)
+{
+	er->csp_lastnodes = NULL;
+}
+static inline void cacheex_free_csp_lastnodes(ECM_REQUEST *er)
+{
+	LLIST *l = er->csp_lastnodes;
+	er->csp_lastnodes = NULL;
+	ll_destroy_data(l);
+}
+static inline void cacheex_set_cacheex_src(ECM_REQUEST *ecm, struct s_client *cl)
+{
+	if (ecm->cacheex_src == cl)
+		ecm->cacheex_src = NULL;
+}
+static inline void cacheex_init_cacheex_src(ECM_REQUEST *ecm, ECM_REQUEST *er)
+{
+	if (!ecm->cacheex_src)
+		ecm->cacheex_src = er->cacheex_src;
+}
 #else
 static inline void cacheex_init(void) { };
 static inline void cacheex_clear_account_stats(struct s_auth *UNUSED(account)) { };
@@ -26,6 +46,11 @@ static inline void cacheex_clear_client_stats(struct s_client *UNUSED(client)) {
 static inline void cacheex_load_config_file(void) { };
 static inline void cacheex_update_hash(ECM_REQUEST *UNUSED(er)) { };
 static inline bool cacheex_reader(struct s_reader *UNUSED(rdr)) { return false; };
+static inline bool cacheex_is_match_alias(struct s_client *UNUSED(cl), ECM_REQUEST *UNUSED(er)) { return false; }
+static inline void cacheex_set_csp_lastnode(ECM_REQUEST *UNUSED(er)) { }
+static inline void cacheex_free_csp_lastnodes(ECM_REQUEST *UNUSED(er)) { }
+static inline void cacheex_set_cacheex_src(ECM_REQUEST *UNUSED(ecm), struct s_client *UNUSED(cl)) { }
+static inline void cacheex_init_cacheex_src(ECM_REQUEST *UNUSED(ecm), ECM_REQUEST *UNUSED(er)) { }
 #endif
 
 #endif

commit 02021ee685083afb303bd2b0e2db72450f6b10eb

cacheex: Fix crash when freeing ECM.
Fix a dumb bug introduced in r7674.

git-svn-id: http://www.streamboard.tv/svn/oscam/trunk@7675 4b0bc96b-bc66-0410-9d44-ebda105a78c1

oscam.c
@@ -320,7 +320,7 @@ static void remove_ecm_from_reader(ECM_REQUEST *ecm) {
 	            	if (er->parent == ecm) {
 	            		er->parent = NULL;
 	            		er->client = NULL;
-	            		cacheex_set_csp_lastnode(NULL);
+	            		cacheex_set_csp_lastnode(er);
 	            	}
 	            }
             }