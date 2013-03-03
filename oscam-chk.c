fairbird/NCam

commit ac0625116820edeb991d812a6b632b2d232b659d

cfg: Remove newcamd vars from struct s_config when the module is disabled.
git-svn-id: http://www.streamboard.tv/svn/oscam/trunk@8465 4b0bc96b-bc66-0410-9d44-ebda105a78c1

oscam-chk.c
@@ -364,6 +364,7 @@ int32_t chk_srvid_by_caid_prov_rdr(struct s_reader *rdr, uint16_t caid, uint32_t
 // server filter for newcamd
 int32_t chk_sfilter(ECM_REQUEST *er, PTAB *ptab)
 {
+#ifdef MODULE_NEWCAMD
   int32_t i, j, pi, rc=1;
   uint16_t caid, scaid;
   uint32_t  prid, sprid;
@@ -409,6 +410,11 @@ int32_t chk_sfilter(ECM_REQUEST *er, PTAB *ptab)
     }
   }
   return (rc);
+#else
+  (void)er;
+  (void)ptab;
+  return 0;
+#endif
 }
 
 static int32_t chk_chid(ECM_REQUEST *er, FTAB *fchid, char *type, char *name)

globals.h
@@ -1660,12 +1660,14 @@ struct s_config
 	IN_ADDR_T		c35_tcp_srvip;
 #endif
 	int8_t			c35_suppresscmd08; // used in cccam module
+#ifdef MODULE_NEWCAMD
 	PTAB			ncd_ptab;
 	IN_ADDR_T		ncd_srvip;
 	uint8_t			ncd_key[14];
 	int8_t			ncd_keepalive;
 	int8_t			ncd_mgclient;
 	struct s_ip 	*ncd_allowed;
+#endif
 #ifdef MODULE_RADEGAST
 	int32_t			rad_port;
 	IN_ADDR_T		rad_srvip;

oscam-conf-mk.c
@@ -286,6 +286,7 @@ char *mk_t_aeskeys(struct s_reader *rdr) {
  * Creates a string ready to write as a token into config or WebIf for the Newcamd Port. You must free the returned value through free_mk_t().
  */
 char *mk_t_newcamd_port(void) {
+#ifdef MODULE_NEWCAMD
 	int32_t i, j, k, pos = 0, needed = 1;
 
 	/* Precheck to determine how long the resulting string will maximally be (might be a little bit smaller but that shouldn't hurt) */
@@ -328,6 +329,9 @@ char *mk_t_newcamd_port(void) {
 		dot1=";";
 	}
 	return value;
+#else
+	return NULL;
+#endif
 }
 
 /*

oscam-config-account.c
@@ -33,11 +33,15 @@ static void account_c35_suppresscmd08_fn(const char *token, char *value, void *s
 
 static void account_ncd_keepalive_fn(const char *token, char *value, void *setting, FILE *f) {
 	int8_t *ncd_keepalive = setting;
+	int8_t def_value = 0;
+#ifdef MODULE_NEWCAMD
+	def_value = cfg.ncd_keepalive;
+#endif
 	if (value) {
-		*ncd_keepalive = (int8_t)strToIntVal(value, cfg.ncd_keepalive);
+		*ncd_keepalive = (int8_t)strToIntVal(value, def_value);
 		return;
 	}
-	if (*ncd_keepalive != cfg.ncd_keepalive || cfg.http_full_cfg)
+	if (*ncd_keepalive != def_value || cfg.http_full_cfg)
 		fprintf_conf(f, token, "%d\n", *ncd_keepalive);
 }
 

oscam-config-global.c
@@ -782,7 +782,9 @@ void config_set(char *section, const char *token, char *value) {
 
 void config_free(void) {
 	config_sections_free(oscam_conf, &cfg);
+#ifdef MODULE_NEWCAMD
 	clear_ptab(&cfg.ncd_ptab);
+#endif
 #ifdef MODULE_CAMD35_TCP
 	clear_ptab(&cfg.c35_tcp_ptab);
 #endif

oscam-ecm.c
@@ -1173,12 +1173,14 @@ void get_cw(struct s_client * client, ECM_REQUEST *er)
 		}
 	}
 
+#ifdef MODULE_NEWCAMD
 	// Set providerid for newcamd clients if none is given
 	if (!er->prid && client->ncd_server) {
 		int32_t pi = client->port_idx;
 		if (pi >= 0 && cfg.ncd_ptab.nports && cfg.ncd_ptab.nports >= pi && cfg.ncd_ptab.ports[pi].ncd)
 			er->prid = cfg.ncd_ptab.ports[pi].ncd->ncd_ftab.filts[0].prids[0];
 	}
+#endif
 
 	// CAID not supported or found
 	if (!er->caid) {

commit 18ae214372a01edf58548259ecde8be0e51a8e49

Fix for regression introduces in r8465.
chk_sfilter should return true.

This should fix ticket #3207.

git-svn-id: http://www.streamboard.tv/svn/oscam/trunk@8469 4b0bc96b-bc66-0410-9d44-ebda105a78c1

oscam-chk.c
@@ -413,7 +413,7 @@ int32_t chk_sfilter(ECM_REQUEST *er, PTAB *ptab)
 #else
   (void)er;
   (void)ptab;
-  return 0;
+  return 1;
 #endif
 }
 