fairbird/NCam

commit f7b5de0ae614fac10da051e590af56cc7d7811e6

WebIf: Livelog - Using an ID for each log entry instead of flagging delivered entries. This avoids gaps in Log if the livelog page is shown in 2 or more webbrowsers simultaniosly. (THX Admin)
If somebody want to use the logpoll JSON for own applications:

1) first call: <oscam_ip>?logpoll.html&lastid=0
2) the returned JSON contains an id in each logentry (oscam.lines[n].id) - use the id of last line for next call

git-svn-id: http://www.streamboard.tv/svn/oscam/trunk@9603 4b0bc96b-bc66-0410-9d44-ebda105a78c1

module-webif.c
@@ -38,6 +38,7 @@ extern int32_t exit_oscam;
 extern char *entitlement_type[];
 extern char *RDR_CD_TXT[];
 extern char *loghist;
+extern char *loghistid;
 extern char *loghistptr;
 
 int32_t ssl_active = 0;
@@ -3914,18 +3915,18 @@ static char *send_oscam_entitlement(struct templatevars *vars, struct uriparams 
 static char *send_oscam_logpoll(struct templatevars * vars, struct uriparams * params)
 {
 
-	int full_log = 0;
+	uint64_t lastid = 0;
 
-	if(strcmp(getParam(params, "lasttime"), "start") == 0){
+	if(strcmp(getParam(params, "lastid"), "start") == 0){
 		setActiveMenu(vars, MNU_LIVELOG); 
 		return tpl_getTpl(vars, "LOGPAGE");
 	}
 	else
 	{
-		full_log = atoi(getParam(params, "lasttime"));
+		lastid = strtoull(getParam(params, "lastid"), NULL, 10);
 	}
 
-	int dot = 0; //Delimiter
+	char *dot = ""; //Delimiter
 
 #ifdef WITH_DEBUG
 	char *debuglvl = getParam(params, "debug");
@@ -3936,28 +3937,26 @@ static char *send_oscam_logpoll(struct templatevars * vars, struct uriparams * p
 			cs_log("%s debug_level=%d", "all", cs_dblevel);
 		}
 	}
-	tpl_printf(vars, TPLAPPEND, "DATA","%s\"debug\":\"%d\"", dot?",":"", cs_dblevel);
-	dot++;
-	tpl_printf(vars, TPLAPPEND, "DATA","%s\"maxdebug\":\"%d\"",dot?",":"", MAX_DEBUG_LEVELS);
+	tpl_printf(vars, TPLAPPEND, "DATA","%s\"debug\":\"%d\"", dot, cs_dblevel);
+	dot = ",";
+	tpl_printf(vars, TPLAPPEND, "DATA","%s\"maxdebug\":\"%d\"",dot, MAX_DEBUG_LEVELS);
 #endif
 
 	if(cfg.loghistorysize == 0){
-		tpl_printf(vars, TPLAPPEND, "DATA","%s\"logdisabled\":\"1\"",dot?",":"");
+		tpl_printf(vars, TPLAPPEND, "DATA","%s\"logdisabled\":\"1\"",dot);
 		return tpl_getTpl(vars, "POLL");
-		dot++;
 	}
 
 	char *t_loghistptr = loghistptr, *ptr1 = NULL;
 	if(loghistptr >= loghist + (cfg.loghistorysize) - 1)
 		{ t_loghistptr = loghist; }
 	
-	int32_t d = 0, l1 = strlen(t_loghistptr + 1) + 2;
+	int32_t i, d = 0, l1 = strlen(t_loghistptr + 1) + 2;
 	char *lastpos = loghist + (cfg.loghistorysize) - 1;
 	
-	tpl_printf(vars, TPLAPPEND, "DATA", "%s\"lines\":[", dot?",":"");
+	tpl_printf(vars, TPLAPPEND, "DATA", "%s\"lines\":[", dot);
 
-	int i;
-	dot = 0;
+	dot = "";
 	for(ptr1 = t_loghistptr + l1, i = 0; i < 200; i++, ptr1 = ptr1 + l1)
 	{
 		l1 = strlen(ptr1) + 1;
@@ -3969,8 +3968,7 @@ static char *send_oscam_logpoll(struct templatevars * vars, struct uriparams * p
 		}
 		
 		if(d && ((ptr1 >= t_loghistptr) || (l1 < 2)))
-			{ break; }
-		
+			{ break; }		
 		
 		char p_usr[32];
 		size_t pos1 = strcspn(ptr1, "\t") + 1;
@@ -3981,14 +3979,16 @@ static char *send_oscam_logpoll(struct templatevars * vars, struct uriparams * p
 		pos1 = strcspn(p_txt, "\n") + 1;
 		char str_out[pos1];
 		cs_strncpy(str_out, p_txt, pos1);
+		uint64_t *id;
+		id = (uint64_t*)(loghistid + (ptr1-loghist)/3);
 
-		if(p_txt[0] && (p_txt[0] == '0' || full_log)){
-			tpl_printf(vars, TPLAPPEND, "DATA","%s{\"usr\":\"%s\",\"line\":\"%s\"}",
-									dot?",":"",
+		if(*id > lastid){
+			tpl_printf(vars, TPLAPPEND, "DATA","%s{\"id\":\"%" PRIu64 "\",\"usr\":\"%s\",\"line\":\"%s\"}",
+									dot,
+									*id,
 									xml_encode(vars, p_usr),
-									xml_encode(vars, str_out + 1));
-			dot = 1; // next in Array with leading delimiter
-			p_txt[0] = '1'; // mark as delivered
+									xml_encode(vars, str_out));
+			dot = ","; // next in Array with leading delimiter
 		}
 	}
 	tpl_addVar(vars, TPLAPPEND, "DATA", "]");
@@ -4745,7 +4745,7 @@ static char *send_oscam_status(struct templatevars * vars, struct uriparams * pa
 				}
 				else
 				{
-					tpl_addVar(vars, TPLAPPEND, "LOGHISTORY", p_txt +1);
+					tpl_addVar(vars, TPLAPPEND, "LOGHISTORY", p_txt);
 				}
 			}
 		}

oscam-log.c
@@ -26,6 +26,7 @@ static int log_list_queued;
 static pthread_t log_thread;
 static pthread_cond_t log_thread_sleep_cond;
 static pthread_mutex_t log_thread_sleep_cond_mutex;
+static uint64_t counter = 0;
 
 struct s_log
 {
@@ -40,6 +41,7 @@ struct s_log
 #if defined(WEBIF) || defined(MODULE_MONITOR)
 static CS_MUTEX_LOCK loghistory_lock;
 char *loghist = NULL;     // ptr of log-history
+char *loghistid = NULL;
 char *loghistptr = NULL;
 #endif
 
@@ -236,13 +238,14 @@ int32_t cs_open_logfiles(void)
 */
 void cs_reinit_loghist(uint32_t size)
 {
-	char *tmp = NULL, *tmp2;
+	char *tmp = NULL, *tmp2, *tmp3 = NULL, *tmp4;
 	if(size != cfg.loghistorysize)
 	{
-		if(size == 0 || cs_malloc(&tmp, size))
+		if(size == 0 || (cs_malloc(&tmp, size) && cs_malloc(&tmp3, size/3+8)))
 		{
 			cs_writelock(&loghistory_lock);
 			tmp2 = loghist;
+			tmp4 = loghistid;
 			// On shrinking, the log is not copied and the order is reversed
 			if(size < cfg.loghistorysize)
 			{
@@ -250,6 +253,7 @@ void cs_reinit_loghist(uint32_t size)
 				cs_sleepms(20); // Monitor or webif may be currently outputting the loghistory but don't use locking so we sleep a bit...
 				loghistptr = tmp;
 				loghist = tmp;
+				loghistid = tmp3;
 			}
 			else
 			{
@@ -257,14 +261,18 @@ void cs_reinit_loghist(uint32_t size)
 				{
 					memcpy(tmp, loghist, cfg.loghistorysize);
 					loghistptr = tmp + (loghistptr - loghist);
+					memcpy(tmp3, loghistid, cfg.loghistorysize/3);
+				} else { 
+					loghistptr = tmp;
 				}
-				else { loghistptr = tmp; }
 				loghist = tmp;
+				loghistid = tmp3;
 				cs_sleepms(20); // Monitor or webif may be currently outputting the loghistory but don't use locking so we sleep a bit...
 				cfg.loghistorysize = size;
 			}
 			cs_writeunlock(&loghistory_lock);
 			if(tmp2 != NULL) { add_garbage(tmp2); }
+			if(tmp4 != NULL) { add_garbage(tmp4); }
 		}
 	}
 }
@@ -321,7 +329,7 @@ static void write_to_log(char *txt, struct s_log *log, int8_t do_flush)
 	{
 		char *usrtxt = log->cl_text;
 		char *target_ptr = NULL;
-		int32_t target_len = strlen(usrtxt) + 1 +(strlen(txt) - 8) + 1;
+		int32_t target_len = strlen(usrtxt) + (strlen(txt) - 8) + 1;
 
 		cs_writelock(&loghistory_lock);
 		char *lastpos = loghist + (cfg.loghistorysize) - 1;
@@ -331,7 +339,7 @@ static void write_to_log(char *txt, struct s_log *log, int8_t do_flush)
 			target_len = strlen(usrtxt) + (strlen(txt) - 8) + 1;
 		}
 		if(!loghistptr)
-			{ loghistptr = loghist; }
+			{ loghistptr = loghist;	}
 
 		if(loghistptr + target_len + 1 > lastpos)
 		{
@@ -346,8 +354,12 @@ static void write_to_log(char *txt, struct s_log *log, int8_t do_flush)
 			loghistptr = loghistptr + target_len + 1;
 			*loghistptr = '\0';
 		}
+		++counter;
 		cs_writeunlock(&loghistory_lock);
-		snprintf(target_ptr, target_len + 1, "%s\t0%s", usrtxt, txt + 8);
+		snprintf(target_ptr, target_len + 1, "%s\t%s", usrtxt, txt + 8);
+		uint64_t *id;
+		id = (uint64_t*)(loghistid + (target_ptr-loghist)/3);
+		*id = counter;
 	}
 #endif
 
@@ -762,6 +774,7 @@ void log_free(void)
 	pthread_join(log_thread, NULL);
 #if defined(WEBIF) || defined(MODULE_MONITOR)
 	NULLFREE(loghist);
-	loghist = loghistptr = NULL;
+	NULLFREE(loghistid);
+	loghist = loghistptr = loghistid = NULL;
 #endif
 }

webif/include/jscript.js
@@ -624,9 +624,8 @@ function updateLogpage(data) {
 				}
 			}
 		}
+		parameters = "?lastid=" + item.id; 
 	});
-
-	parameters = "?lasttime=0";
 	
 	// update footer
 	updateFooter(data);

webif/include/menu.html
@@ -1,7 +1,7 @@
 		<DIV ID="main">
 			<UL ID="mainmenu">
 				<LI CLASS="##MENUACTIVE0##"><A HREF="status.html">Status</A></LI>
-				<LI CLASS="##MENUACTIVE10##"><A TITLE="Move mouse over log-window to stop scroll" HREF="logpoll.html?lasttime=start">Live Log</A></LI>
+				<LI CLASS="##MENUACTIVE10##"><A TITLE="Move mouse over log-window to stop scroll" HREF="logpoll.html?lastid=start">Live Log</A></LI>
 				<LI CLASS="##MENUACTIVE1##"><A HREF="config.html">Config</A></LI>
 				<LI CLASS="##MENUACTIVE2##"><A HREF="readers.html">Readers</A></LI>
 				<LI CLASS="##MENUACTIVE3##"><A HREF="userconfig.html">Users</A></LI>

webif/logpage/logpage.html
@@ -8,7 +8,7 @@
 
 	var page = 'livelog';
 	var jsonurl = 'logpoll.html';
-	var parameters = "?lasttime=1";
+	var parameters = "?lastid=0";
 	var lastdebuglevel = 0;
 	var stoppoll = 0;
 

commit 0af434780365dd585e44ee7585fe9ed63a2c334f

Hopefully fix alignment issue# introduced in r9603 for some platforms.
git-svn-id: http://www.streamboard.tv/svn/oscam/trunk@9631 4b0bc96b-bc66-0410-9d44-ebda105a78c1

module-webif.c
@@ -3998,13 +3998,12 @@ static char *send_oscam_logpoll(struct templatevars * vars, struct uriparams * p
 		pos1 = strcspn(p_txt, "\n") + 1;
 		char str_out[pos1];
 		cs_strncpy(str_out, p_txt, pos1);
-		uint64_t *id;
-		id = (uint64_t*)(loghistid + (ptr1-loghist)/3);
+		uint64_t id = b2ll(8, (uchar *) (loghistid + ((ptr1-loghist)/3)));
 
-		if(*id > lastid){
+		if(id > lastid){
 			tpl_printf(vars, TPLAPPEND, "DATA","%s{\"id\":\"%" PRIu64 "\",\"usr\":\"%s\",\"line\":\"%s\"}",
 									dot,
-									*id,
+									id,
 									xml_encode(vars, p_usr),
 									xml_encode(vars, str_out));
 			dot = ","; // next in Array with leading delimiter

oscam-log.c
@@ -360,9 +360,7 @@ static void write_to_log(char *txt, struct s_log *log, int8_t do_flush)
 		++counter;
 		cs_writeunlock(&loghistory_lock);
 		snprintf(target_ptr, target_len + 1, "%s\t%s", usrtxt, txt + 8);
-		uint64_t *id;
-		id = (uint64_t*)(loghistid + (target_ptr-loghist)/3);
-		*id = counter;
+		ull2b_buf(counter, (uchar *)(loghistid + ((target_ptr-loghist)/3)));
 	}
 #endif
 

oscam-string.c
@@ -312,6 +312,18 @@ uchar *i2b_buf(int32_t n, uint32_t i, uchar *b)
 	return b;
 }
 
+void ull2b_buf(uint64_t i, uchar *b)
+{
+	b[0] = (i >> 56) & 0xff;
+	b[1] = (i >> 48) & 0xff;
+	b[2] = (i >> 40) & 0xff;
+	b[3] = (i >> 32) & 0xff;
+	b[4] = (i >> 24) & 0xff;
+	b[5] = (i >> 16) & 0xff;
+	b[6] = (i >> 8) & 0xff;
+	b[7] = (i) & 0xff;
+}
+
 uint32_t a2i(char *asc, int32_t bytes)
 {
 	int32_t i, n;

oscam-string.h
@@ -25,6 +25,7 @@ int32_t key_atob_l(char *asc, uchar *bin, int32_t l);
 uint32_t b2i(int32_t n, const uchar *b);
 uint64_t b2ll(int32_t n, uchar *b);
 uchar *i2b_buf(int32_t n, uint32_t i, uchar *b);
+void ull2b_buf(uint64_t i, uchar *b);
 uint32_t a2i(char *asc, int32_t bytes);
 
 int32_t boundary(int32_t exp, int32_t n);