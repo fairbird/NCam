73c0d849a613d5a0860ffba7df887d3c1c0e2c38
fairbird <rrrr53@hotmail.com>
2020-11-04 18:19:55 +0300

    Need PIN on 092F and 091F caid's fixed

--- reader-videoguard2.c
+++ reader-videoguard2.c
@@ -1217,6 +1217,10 @@ static int32_t videoguard2_do_ecm(struct s_reader *reader, const ECM_REQUEST *er
 		memmove(tbuff + 11, tbuff + 13, new_len - 11);
 		new_len -= 2;
 	}
+	else if(reader->caid == 0x91F || reader->caid == 0x92F)
+	{
+		ins40[2] = 0x60;
+	}
 
 	ins40[4] = new_len;
 	int32_t l;
