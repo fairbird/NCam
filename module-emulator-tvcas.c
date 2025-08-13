0f2e81835c00b3a9a169f281194c75e6b7bc5f89
fairbird <rrrr53@hotmail.com>
2025-08-14 00:10:07 +0100

    TVCAS: Updating the checksum and other codes

--- module-emulator-tvcas.c
+++ module-emulator-tvcas.c
@@ -7,10 +7,20 @@
 #include "module-emulator-nemu.h"
 #include "ncam-string.h"
 
+static uint8_t csum(uint8_t *bytes, uint8_t len)
+{
+	uint8_t i, cs = 0;
+	for(i = 0; i < len; i++)
+	{
+		cs += bytes[i];
+	}
+	return cs;
+}
+
 int8_t conax_ecm(uint16_t caid, uint8_t *ecm, uint8_t *dw)
 {
-	uint32_t i, ks1[32], ks2[32];
-	uint8_t key[16], keyIndex = ecm[0] & 3, ecm_len = ecm[4] - 2, nano = ecm[5], *ecm_data = ecm + 7;
+	uint32_t ks1[32], ks2[32];
+	uint8_t i, key[16], keyIndex = ecm[0] & 3, ecm_len = ecm[4] - 2, nano = ecm[5], *ecm_data = ecm + 7;
 
 	/* TVCAS3 test ... */
 	if(ecm_len != 48 || nano != 0x64)
@@ -31,26 +41,35 @@ int8_t conax_ecm(uint16_t caid, uint8_t *ecm, uint8_t *dw)
 	des_set_key(key, ks1);
 	des_set_key(key + 8, ks2);
 
-	cs_log_dump_dbg(D_ATR | D_READER, ecm_data, 24, "Encrypted ECM segment:");
+	cs_log_dump_dbg(D_ATR | D_READER, ecm_data, ecm_len, "Encrypted ECM segment:");
 
-	for(i = 0; i < 17; i += 8)
+	for(i = 0; i < ecm_len; i += 8)
 	{
 		des(ecm_data + i, ks1, 0);
 		des(ecm_data + i, ks2, 1);
 		des(ecm_data + i, ks1, 0);
 	}
 
-	cs_log_dump_dbg(D_ATR | D_READER, ecm_data, 24, "Decrypted ECM segment:");
-
+	cs_log_dump_dbg(D_ATR | D_READER, ecm_data, ecm_len, "Decrypted ECM segment:");
+/*
+	time_t timestamp = (time_t)b2i(4, ecm_data);
+	struct tm tm_info;
+	localtime_r(&timestamp, &tm_info);
+	cs_log_dbg(D_ATR | D_READER, "Timestamp: %04d-%02d-%02d %02d:%02d:%02d, Access Criteria: %08X",
+		tm_info.tm_year + 1900,
+		tm_info.tm_mon + 1,
+		tm_info.tm_mday,
+		tm_info.tm_hour,
+		tm_info.tm_min,
+		tm_info.tm_sec,
+		b2i(4, ecm_data + 20));
+*/
 	memcpy(dw + 8, ecm_data + 4, 8);
 	memcpy(dw, ecm_data + 12, 8);
 
 	cs_log_dump_dbg(D_ATR | D_READER, dw, 16, "Control Word:");
-#if 0
-	return EMU_OK;
-#else
-	return (is_valid_dcw(dw) && is_valid_dcw(dw + 8)) ? EMU_OK : EMU_CHECKSUM_ERROR;
-#endif
+
+	return (ecm_data[ecm_len - 1] == csum(ecm_data, ecm_len - 1)) ? EMU_OK : EMU_CHECKSUM_ERROR;
 }
 
 #endif
