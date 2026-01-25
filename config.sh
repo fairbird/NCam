fairbird/NCam

commit 9a5d4d07d4ad049a6bb0278a342437e7289c135d

build: fix is_defined.txt generation for proper webif template filtering The is_defined.txt file is used by pages_gen to filter templates based on enabled config options. Previously, this file was only created when explicitly calling config.sh with --enable/--disable/--restore, but not during normal builds.
The 'webif clean' added in 96debcd7 ('Fix webif compression parameter') ensured pages.c was always rebuilt, but also deleted is_defined.txt which broke the filtering mechanism.

Changes:
- Call write_enabled() in make_config_mak() to ensure is_defined.txt is generated on every build
- Remove redundant 'webif clean' from Makefile and CMakeLists.txt (write_enabled already deletes pages.c when needed)
- Add config.h dependency to pages.c target to trigger rebuild when config options change

This ensures proper filtering works for:
- Fresh checkouts with default config
- Builds after direct config.h modifications
- Builds after config.sh --enable/--disable changes

cherry-pick > https://git.streamboard.tv/common/oscam/-/commit/5b7ec3c95e673879499853af5c37fba4097c631d

config.sh
@@ -462,6 +462,7 @@ make_config_mak() {
 		[ $? != 0 ] && cat $TMPFILE > $OBJDIR/config.c
 	fi
 	rm -rf $TMPFILE
+	write_enabled
 }
 
 check_test() {

CMakeLists.txt
@@ -756,7 +756,6 @@ GENERATE_NCAM_CONFIG("${CMAKE_CURRENT_SOURCE_DIR}/config.h")
 execute_process (COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/config.sh --objdir ${CMAKE_CURRENT_BINARY_DIR} --use-flags  "${USE_STAPI} ${USE_STAPI5} /
 ${USE_COOLAPI} ${USE_SU980} ${USE_WETEK} ${USE_AZBOX} ${USE_AMSMC} ${USE_GXAPI} ${USE_MCA} ${USE_SSL} ${USE_LIBCRYPTO} ${USE_LIBUSB} ${USE_PCSC}  ${USE_COMPRESS_FLAG} ${USE_LIBCURL}" --make-config.mak)
 # Generate webif/pages.c
-execute_process (COMMAND make --no-print-directory --quiet -C ${CMAKE_CURRENT_SOURCE_DIR}/webif clean)
 execute_process (COMMAND make --no-print-directory --quiet -C ${CMAKE_CURRENT_SOURCE_DIR}/webif)
 
 #----------------------- subdirectories ------------------------------

webif/Makefile
@@ -14,7 +14,7 @@ endif
 
 all: pages.c
 
-pages.c: pages_gen
+pages.c: pages_gen ../config.h
 	$(SAY) "GEN	webif/$@"
 	$(Q)./pages_mkdep
 	$(Q)./pages_gen

commit fa68e614722fc8368bb3f44771e96f48082a6e2b

webif: fix PCSC devices not shown on scanusb.html The write_enabled() function generates is_defined.txt which pages_gen uses to filter templates based on enabled config options. However, it only wrote config.h defines and missed USE_FLAG based card readers. CARDREADER_PCSC is controlled via USE_PCSC flag passed at build time, not a config.h define. Since it was never written to is_defined.txt, pages_gen filtered out scanusb_pcscbit.html template, causing PCSC devices to not appear on scanusb.html. Fixes regression introduced in https://github.com/fairbird/NCam/commit/9a5d4d07d4ad049a6bb0278a342437e7289c135d
cherry-pick > https://git.streamboard.tv/common/oscam/-/commit/98e5f92f229edd29f84a20859ca074d71a750524

config.sh
@@ -282,6 +282,8 @@ write_enabled() {
 	for OPT in $(get_opts) WITH_CARDREADER
 	do
 		enabled $OPT && printf "%s\n" $OPT >> $defined_file
+		# Handle USE_FLAG based card readers for webif template filtering
+		have_flag USE_PCSC && printf "%s\n" CARDREADER_PCSC >> $defined_file
 	done
 }
 